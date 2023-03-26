12e314bdbea486d44aa53bd26037008274901af0
unknown <patinya.y@ku.th>
2023-03-27 03:37:04 +0700

    delete dubug

--- src/main/java/com/payment/paymentAPI/queryresolvers/PaymentMutation.java
+++ src/main/java/com/payment/paymentAPI/queryresolvers/PaymentMutation.java
@@ -3,10 +3,8 @@ package com.payment.paymentAPI.queryresolvers;
 import com.payment.paymentAPI.service.PaymentService;
 import com.payment.paymentAPI.types.NewPayment;
 import com.payment.paymentAPI.types.PaymentInput;
-import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
@@ -19,14 +17,6 @@ import javax.sql.DataSource;
 public class PaymentMutation {
 
     PaymentService paymentService;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @PostConstruct
-    public void printDataSource() {
-        System.out.println("Datasource :"+dataSource);
-    }
 
     @MutationMapping
     public NewPayment createPayment(@Argument("input") PaymentInput paymentInput) {
